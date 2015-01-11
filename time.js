6ac9136f89332b67071802c30c18d7f9613eab04
aymarino <aymarino@0587380108.wireless.umich.net>
2015-01-11 14:48:12 -0500

    Initial commit

---
+++ time.js
@@ -0,0 +1,37 @@
+function dotime(){
+  $("body").css({"transition": "all 0.8s", "-webkit-transition": "all 0.8s"});
+  
+  var d = new Date();
+  var date = d.toDateString();
+
+  var hours = d.getHours(); var pm = false;
+  if (hours > 12) {
+    pm = true;
+    var americanHours = hours - 12;
+  }
+  
+  var mins = d.getMinutes();
+  var secs = d.getSeconds();
+  
+  if (hours < 10){hours = "0" + hours};
+  if (mins < 10){mins = "0" + mins};
+  if (secs < 10){secs = "0" + secs};
+  
+  hours.toString();
+  mins.toString();
+  secs.toString();
+  
+  var hex = "#" + hours + mins + secs;
+  
+  $("#d").html(date);
+
+  if (pm) $("#t").html(americanHours + " : " + mins + " : " + secs + "&nbsp; PM");
+  else $("#t").html(hours + " : " + mins +" : " + secs + "&nbsp; AM");
+  $("#h").html(hex);
+  
+  document.body.style.background = hex;
+  
+  setTimeout(function(){ dotime();}, 1000);
+}
+
+window.onload = dotime;
